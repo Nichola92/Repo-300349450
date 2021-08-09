cf8729376232a4f8404dfe6a85f7f963761dd4cc
Ayodhni Muthuarachchi <muthuarachchim@student.douglascollege.ca>
2021-08-09 13:14:48 -0700

    first commit

---
+++ script/script.js
@@ -0,0 +1,100 @@
+function openProfile(){
+  // Get the modal
+  var modal = document.getElementById("myModal");
+
+  // Get the image and insert it inside the modal
+  var img = document.getElementById("profile");
+  var modalImg = document.getElementById("img01");
+
+  modal.style.display = "block";
+  modalImg.src = img.src;
+
+}
+
+function openMarketing(){
+  // Get the modal
+  var modal = document.getElementById("myModal");
+
+  // Get the image and insert it inside the modal
+  var img = document.getElementById("marketing");
+  var modalImg = document.getElementById("img01");
+
+  modal.style.display = "block";
+  modalImg.src = img.src;
+
+}
+
+function openSearchPage(){
+  // Get the modal
+  var modal = document.getElementById("myModal");
+
+  // Get the image and insert it inside the modal
+  var img = document.getElementById("searchPage");
+  var modalImg = document.getElementById("img01");
+
+  modal.style.display = "block";
+  modalImg.src = img.src;
+
+}
+
+function openTravelApp(){
+  // Get the modal
+  var modal = document.getElementById("myModal");
+
+  // Get the image and insert it inside the modal
+  var img = document.getElementById("travelApp");
+  var modalImg = document.getElementById("img01");
+
+  modal.style.display = "block";
+  modalImg.src = img.src;
+
+}
+
+function openFavoriteSpots(){
+  // Get the modal
+  var modal = document.getElementById("myModal");
+
+  // Get the image and insert it inside the modal
+  var img = document.getElementById("favoriteSpots");
+  var modalImg = document.getElementById("img01");
+
+  modal.style.display = "block";
+  modalImg.src = img.src;
+
+}
+
+function openPhotoGallery(){
+  // Get the modal
+  var modal = document.getElementById("myModal");
+
+  // Get the image and insert it inside the modal
+  var img = document.getElementById("photoGallery");
+  var modalImg = document.getElementById("img01");
+
+  modal.style.display = "block";
+  modalImg.src = img.src;
+
+}
+
+function openCalculator(){
+  // Get the modal
+  var modal = document.getElementById("myModal");
+
+  // Get the image and insert it inside the modal
+  var img = document.getElementById("calculator");
+  var modalImg = document.getElementById("img01");
+
+  modal.style.display = "block";
+  modalImg.src = img.src;
+
+}
+
+
+// When the user clicks on <span> (x), close the modal
+function closeModal() { 
+  // Get the <span> element that closes the modal
+  var modal = document.getElementById("myModal");
+  modal.style.display = "none";
+
+
+}
